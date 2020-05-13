6423dd105af743fcd46349eb108e561db9c9d810
BlueBogus <adonis.marsas@gmail.com>
2020-05-13 12:14:30 +0300

    second

--- script.js
+++ script.js
@@ -3,13 +3,15 @@
 const body = document.querySelector('body');
 const popup = document.querySelector('.popup-container');
 
-body.addEventListener('mouseleave', e => {
+body.addEventListener('mouseleave', showPopup);
+
+function showPopup(event) {
   popup.style.display = 'flex';
-});
+}
 
 body.addEventListener('click', e => {
   if(!e.target.closest('.popup')) {
     popup.style.display = 'none';
-    popup.remove();
+    body.removeEventListener('mouseleave', showPopup);
   }
 });
